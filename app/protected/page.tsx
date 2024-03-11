9a043f8ecc9d4430a0bea3fbe1d6217698e4bfd5
Gorcc <gorkem.ater1@hotmail.com>
2024-03-11 14:33:41 +0200

    initial commit

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -19,13 +19,13 @@ export default async function ProtectedPage() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="w-full">
-        <div className="py-6 font-bold bg-purple-950 text-center">
+        {/* <div className="py-6 font-bold bg-purple-950 text-center">
           This is a protected page that you can only see as an authenticated
           user
-        </div>
+        </div> */}
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <DeployButton />
+            <h1>Marketplace App</h1>
             <AuthButton />
           </div>
         </nav>
@@ -34,8 +34,7 @@ export default async function ProtectedPage() {
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          <FetchDataSteps />
+          <h2 className="font-bold text-4xl mb-4">Currently we are working on the app</h2>
         </main>
       </div>
 
